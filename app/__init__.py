a5015ffa715293c29f8b47105f0b9041c47959b6
isaiah <isasolo44@gmail.com>
2025-05-01 23:25:34 -0700

    renderer fix

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'your-secret-key'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///yourdatabase.db'
-    
+
     db.init_app(app)
     login_manager.init_app(app)
     socketio.init_app(app)
--- app/models.py
+++ app/models.py
@@ -1,5 +1,4 @@
 from app.extensions import db, login_manager
-
 from flask_login import UserMixin
 
 class User(UserMixin, db.Model):
